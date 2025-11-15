17351b2e8c71c80de965c45f72661d23558c689d
Zihan Lu <wilsonlzh@foxmail.com>
2025-11-15 18:38:13 -0500

    Completed all the entities.

--- src/main/java/entity/BlackjackGame.java
+++ src/main/java/entity/BlackjackGame.java
@@ -1,4 +1,52 @@
 package entity;
 
 public class BlackjackGame {
+    private String state;
+    private String deckID;
+    private final BlackjackDealer dealer;
+    private final BlackjackPlayer player;
+    private float betAmount;
+    private float odds;
+    private String result;
+
+    public BlackjackGame(String deckID, BlackjackDealer dealer, BlackjackPlayer player) {
+        this.deckID = deckID;
+        this.dealer = dealer;
+        this.player = player;
+        this.betAmount = 0;
+        this.odds = 1;
+        this.result = "InGame";
+    }
+
+    public String getState() { return state; }
+    public String getDeckID() { return deckID; }
+    public BlackjackDealer getDealer() { return dealer; }
+    public BlackjackPlayer getPlayer() { return player; }
+    public float getBetAmount() { return betAmount; }
+    public float getOdds() { return odds; }
+    public String getResult() { return result; }
+
+    public void setDeckID(String deckID) { this.deckID = deckID; }
+    public void setBetAmount(float betAmount) { this.betAmount = betAmount; }
+    public void setOdds(float odds) { this.odds = odds; }
+    public void setResult(String result) { this.result = result; }
+
+    public void toDealerTurn() { state = "DealerTurn"; }
+    public void toPlayerTurn() { state = "PlayerTurn"; }
+    public void gameOver() { state = "GameOver"; }
+
+    public void playerWin() {
+        this.gameOver();
+        result = "PlayerWin";
+    }
+
+    public void push() {
+        this.gameOver();
+        result = "Push";
+    }
+
+    public void playerLose() {
+        this.gameOver();
+        result = "PlayerLose";
+    }
 }
