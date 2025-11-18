15795d753196fbb5c36f349ea87d252ab0903b36
Zihan Lu <wilsonlzh@foxmail.com>
2025-11-18 18:26:20 -0500

    Completed all files about Hit other than Presenter.

--- src/main/java/use_case/playerHit/PlayerHitInteractor.java
+++ src/main/java/use_case/playerHit/PlayerHitInteractor.java
@@ -1,4 +1,35 @@
 package use_case.playerHit;
 
+import entity.BlackjackPlayer;
+import entity.Card;
+import entity.Hand;
+
 public class PlayerHitInteractor {
+
+    private final PlayerHitUserDataAccessInterface deck;
+    private final PlayerHitOutputBoundary presenter;
+
+    public PlayerHitInteractor(PlayerHitUserDataAccessInterface deck, PlayerHitOutputBoundary presenter) {
+        this.deck = deck;
+        this.presenter = presenter;
+    }
+
+    public void execute(PlayerHitInputData inputData) {
+        BlackjackPlayer player = inputData.getBlackjackPlayer();
+        Hand currentHand = getCurrentHand(inputData);
+
+        Card newCard = deck.drawCard();
+        currentHand.addCard(newCard);
+
+        PlayerHitOutputData outputData = new PlayerHitOutputData(currentHand);
+        presenter.present(outputData);
+    }
+
+    public Hand getCurrentHand(PlayerHitInputData inputData) {
+        BlackjackPlayer player = inputData.getBlackjackPlayer();
+        if (inputData.isInSplittedHand()) {
+            return player.getHands().get(1);
+        }
+        else { return player.getHands().get(0); }
+    }
 }
--- src/main/java/use_case/playerHit/PlayerHitOutputBoundary.java
+++ src/main/java/use_case/playerHit/PlayerHitOutputBoundary.java
@@ -1,4 +1,5 @@
 package use_case.playerHit;
 
 public interface PlayerHitOutputBoundary {
+    void present(PlayerHitOutputData outputData);
 }
