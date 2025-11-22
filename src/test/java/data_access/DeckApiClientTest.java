5940cb8ebcce3ea5c326bbb92f9583483a7ff4b9
SelimAffes-uoft <selim.affes@mail.utoronto.ca>
2025-11-22 12:43:22 -0500

    idk what exactly change here tbh ig I just didn't commit this change

--- src/test/java/data_access/DeckApiClientTest.java
+++ src/test/java/data_access/DeckApiClientTest.java
@@ -4,39 +4,45 @@ import entity.Card;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.List;
-import static org.junit.jupiter.api.Assertions.*;
 
 
-public class DeckInteractionAPITest {
+public class DeckApiClientTest {
     @Test
     void testDeckInteractionAPI() throws IOException {
-        DeckInteractionAPI apiInteractor = new DeckInteractionAPI();
-        String deck1 = apiInteractor.createDeck(1, true, true);
-        String deck2 = apiInteractor.createDeck(1, false, false);
+        DeckApiClient apiClient = new DeckApiClient();
+        String deck1 = apiClient.createDeck(1, true, true);
+        String deck2 = apiClient.createDeck(1, false, false);
         System.out.print(deck1+","+deck2);
     }
 
     @Test
     void testError() throws IOException {
-        DeckInteractionAPI apiInteractor = new DeckInteractionAPI();
-        Card card = apiInteractor.drawCard("a28jd973");
+        DeckApiClient apiClient = new DeckApiClient();
+        Card card = apiClient.drawCard("a28jd973");
     }
 
     @Test
     void testDrawEmptyDeck() throws IOException {
-        DeckInteractionAPI apiInteractor = new DeckInteractionAPI();
-        String deckId = apiInteractor.createDeck(1, true, true);
+        DeckApiClient apiClient = new DeckApiClient();
+        String deckId = apiClient.createDeck(1, true, true);
         for (int i=0; i<56;i++) {
-            Card card = apiInteractor.drawCard(deckId);
+            Card card = apiClient.drawCard(deckId);
             System.out.print(card.getCode());
         }
     }
 
     @Test
     void testDrawCard() throws IOException {
-        DeckInteractionAPI apiInteractor = new DeckInteractionAPI();
-        Card card = apiInteractor.drawCard("rp7zhzmc2nbf");
+        DeckApiClient apiClient = new DeckApiClient();
+        Card card = apiClient.drawCard("rp7zhzmc2nbf");
         System.out.print(card.getCode());
+    }
+
+    @Test
+    void testPileAddCard() throws IOException {
+        DeckApiClient apiClient = new DeckApiClient();
+        Card card = apiClient.drawCard("rp7zhzmc2nbf");
+        apiClient.addCard("rp7zhzmc2nbf", "testPile", card);
+        apiClient.listPile("rp7zhzmc2nbf", "testPile");
     }
 }
