6f46f919872a909aa5acc629c463f38940588046
SelimAffes-uoft <selim.affes@mail.utoronto.ca>
2025-11-23 12:25:42 -0500

    LaunchInputData finished I think

--- src/main/java/use_case/launch/LaunchInputData.java
+++ src/main/java/use_case/launch/LaunchInputData.java
@@ -1,4 +1,12 @@
 package use_case.launch;
 
 public class LaunchInputData {
+
+    private final String destinationPage;
+
+    public LaunchInputData(String destinationPage) {
+        this.destinationPage = destinationPage;
+    }
+
+    String getDestinationPage() { return destinationPage; }
 }
