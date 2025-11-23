fd8d7ce9d28666438214d1479872d5deb0d30bf2
SelimAffes-uoft <selim.affes@mail.utoronto.ca>
2025-11-23 12:40:55 -0500

    LaunchOutputData finished I think

--- src/main/java/use_case/launch/LaunchOutputData.java
+++ src/main/java/use_case/launch/LaunchOutputData.java
@@ -1,4 +1,10 @@
 package use_case.launch;
 
 public class LaunchOutputData {
+
+    private final String destinationPage;
+
+    public LaunchOutputData(String destinationPage) { this.destinationPage = destinationPage; }
+
+    public String getDestinationPage() { return destinationPage; }
 }
