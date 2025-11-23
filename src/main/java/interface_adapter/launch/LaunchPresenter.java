789db4438ab2592ad9811c1553bbcc7b57212265
SelimAffes-uoft <selim.affes@mail.utoronto.ca>
2025-11-23 13:37:04 -0500

    LaunchPresenter done I think

--- src/main/java/interface_adapter/launch/LaunchPresenter.java
+++ src/main/java/interface_adapter/launch/LaunchPresenter.java
@@ -1,4 +1,31 @@
 package interface_adapter.launch;
 
-public class LaunchPresenter {
+import interface_adapter.ViewManagerModel;
+import interface_adapter.login.LoginViewModel;
+import interface_adapter.signup.SignupViewModel;
+import use_case.launch.LaunchOutputBoundary;
+import view.LaunchView;
+
+public class LaunchPresenter implements LaunchOutputBoundary {
+    private final ViewManagerModel viewManagerModel;
+    private final LoginViewModel loginViewModel;
+    private final SignupViewModel signupViewModel;
+
+    public LaunchPresenter(ViewManagerModel viewManagerModel,
+                           LoginViewModel loginViewModel,
+                           SignupViewModel signupViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.loginViewModel = loginViewModel;
+        this.signupViewModel = signupViewModel;
+    }
+
+    public void switchToSignUpView() {
+        viewManagerModel.setState(signupViewModel.getViewName());
+        viewManagerModel.firePropertyChange();
+    }
+
+    public void switchToLogInView() {
+        viewManagerModel.setState(loginViewModel.getViewName());
+        viewManagerModel.firePropertyChange();
+    }
 }
