3ccfd01a8ae53fcd666d718c1e0e3c4cbc137d14
SelimAffes-uoft <selim.affes@mail.utoronto.ca>
2025-11-23 12:38:58 -0500

    LaunchOutputBoundary finished I think

--- src/main/java/use_case/launch/LaunchOutputBoundary.java
+++ src/main/java/use_case/launch/LaunchOutputBoundary.java
@@ -1,4 +1,15 @@
 package use_case.launch;
 
 public interface LaunchOutputBoundary {
+    /**
+     * Prepares the success view for the Launch Use Case
+     * @param outputData the output data
+     */
+    void prepareSuccessView(LaunchOutputData outputData);
+
+    /**
+     * Prepares the failure view for the Launch Use Case
+     * @param errorMessage the explanation of the failure
+     */
+    void prepareFailView(String errorMessage);
 }
