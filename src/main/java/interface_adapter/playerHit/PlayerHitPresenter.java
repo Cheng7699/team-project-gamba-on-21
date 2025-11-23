f0971560a5b31de5b576a78fb24d01a047944378
Zihan Lu <wilsonlzh@foxmail.com>
2025-11-22 23:27:48 -0500

    Completed presenter and added two helper functions to BlackjackView.

--- src/main/java/interface_adapter/playerHit/PlayerHitPresenter.java
+++ src/main/java/interface_adapter/playerHit/PlayerHitPresenter.java
@@ -1,4 +1,28 @@
 package interface_adapter.playerHit;
 
-public class PlayerHitPresenter {
+import entity.BlackjackGame;
+import entity.Hand;
+import use_case.playerHit.PlayerHitOutputBoundary;
+import use_case.playerHit.PlayerHitOutputData;
+import view.BlackjackView;
+
+public class PlayerHitPresenter implements PlayerHitOutputBoundary {
+
+    private BlackjackView view;
+
+    public PlayerHitPresenter(BlackjackView view) {
+        this.view = view;
+    }
+
+    @Override
+    public void present(PlayerHitOutputData outputData) {
+
+        Hand playerHand = outputData.getHandAfterHit();
+        Hand dealerHand = view.getDealerHand();
+        boolean isHideFirstCard = view.isHideDealerHoleCard();
+
+        view.setHands(playerHand, dealerHand, isHideFirstCard);
+
+        if (playerHand.isBust()) { view.showRoundResult("Player Busted!"); }
+    }
 }
