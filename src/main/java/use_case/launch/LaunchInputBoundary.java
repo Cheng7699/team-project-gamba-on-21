14df9797d4f8179fc2192a5b59f9bda1ec2c43b0
SelimAffes-uoft <selim.affes@mail.utoronto.ca>
2025-11-23 12:23:30 -0500

    LaunchInputBoundary done I think

--- src/main/java/use_case/launch/LaunchInputBoundary.java
+++ src/main/java/use_case/launch/LaunchInputBoundary.java
@@ -1,4 +1,13 @@
 package use_case.launch;
 
+/**
+ * Input Boundary for actions which are related to the launch screen in.
+ */
 public interface LaunchInputBoundary {
+
+        /**
+         * Executes the launch (screen) use case.
+         *
+         */
+        void execute();
 }
