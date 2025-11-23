6971b604671802085793976f64b2a863a3074e80
SelimAffes-uoft <selim.affes@mail.utoronto.ca>
2025-11-23 13:01:23 -0500

    added page-switch funcs

--- src/main/java/use_case/launch/LaunchInteractor.java
+++ src/main/java/use_case/launch/LaunchInteractor.java
@@ -1,4 +1,15 @@
 package use_case.launch;
 
-public class LoginInteractor {
+import use_case.login.LoginOutputBoundary;
+
+public class LaunchInteractor implements LaunchInputBoundary{
+    private final LoginOutputBoundary launchPresenter;
+
+    public LaunchInteractor(LoginOutputBoundary launchPresenter){ this.launchPresenter = launchPresenter;}
+
+    @Override
+    public void switchToLogIn() { launchPresenter.switchToLogin(); }
+
+    @Override
+    public void switchToSignUp() { launchPresenter.switchToSignUp(); }
 }
