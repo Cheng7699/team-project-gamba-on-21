b8d6af2787a9f0fe19918b9ef9da140d761a8d19
sean <sean.ma456@gmail.com>
2025-11-29 23:33:56 -0500

    interface adapter stuff

---
+++ src/main/java/interface_adapter/payout/PayoutPresenter.java
@@ -0,0 +1,27 @@
+package interface_adapter.payout;
+
+import interface_adapter.logged_in.LoggedInState;
+import interface_adapter.logged_in.LoggedInViewModel;
+import use_case.payout.PayoutOutputBoundary;
+import use_case.payout.PayoutOutputData;
+
+/**
+ * Presenter for payout use case.
+ */
+public class PayoutPresenter implements PayoutOutputBoundary {
+    private final LoggedInViewModel loggedInViewModel;
+
+    public PayoutPresenter(LoggedInViewModel loggedInViewModel) {
+        this.loggedInViewModel = loggedInViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(PayoutOutputData outputData) {
+        // update balance in logged in state
+        LoggedInState state = loggedInViewModel.getState();
+        state.setBalance(outputData.getNewBalance());
+        loggedInViewModel.setState(state);
+        loggedInViewModel.firePropertyChange("balance");
+    }
+}
+
