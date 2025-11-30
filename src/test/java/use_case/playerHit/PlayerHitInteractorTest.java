2316ba5417958c2159fc3ac4384d7daafa0cd126
Zihan Lu <wilsonlzh@foxmail.com>
2025-11-30 14:09:03 -0500

    Adjusted the Interactor test.

--- src/test/java/use_case/playerHit/PlayerHitInteractorTest.java
+++ src/test/java/use_case/playerHit/PlayerHitInteractorTest.java
@@ -370,4 +370,82 @@ public class PlayerHitInteractorTest {
         assertEquals("PlayerLose", game.getResult());
         assertFalse(presenter.outputData.isSplitHand());
     }
+
+    @Test
+    void testFirstHandBustInSplitGame_DoesNotSetPlayerLose() {
+
+        BlackjackPlayer player = initiatePlayerWithTwoHands();
+        Hand firstHand = player.getHands().get(0);
+        Hand secondHand = player.getHands().get(1);
+
+        BlackjackGame game = createGame(player);
+        game.setSplitted(true);
+
+        Card cardKingDiamonds = new Card("KD", "TestUrl", "KING", "DIAMONDS");
+        Card cardQueenHearts = new Card("QH", "TestUrl", "QUEEN", "HEARTS");
+        firstHand.addCard(cardKingDiamonds);
+        firstHand.addCard(cardQueenHearts);
+
+        assertEquals(20, firstHand.getHandTotalNumber());
+        assertFalse(firstHand.isBust());
+        assertEquals("InGame", game.getResult());
+
+        Card cardTwoClubs = new Card("2C", "TestUrl", "2", "CLUBS");
+        MockDeck deck = new MockDeck(cardTwoClubs);
+        MockPresenter presenter = new MockPresenter();
+
+        PlayerHitInteractor interactor = new PlayerHitInteractor(deck, presenter, game);
+        PlayerHitInputData inputData = new PlayerHitInputData(player, false);
+
+        interactor.execute(inputData);
+
+        assertTrue(deck.drawCalled);
+        assertTrue(presenter.presentCalled);
+        assertSame(firstHand, presenter.outputData.getHandAfterHit());
+
+        assertTrue(firstHand.isBust());
+        assertTrue(presenter.outputData.isBust());
+        assertFalse(presenter.outputData.isSplitHand());
+
+        assertEquals("InGame", game.getResult());
+    }
+
+    @Test
+    void testSplitHandBustInSplitGame_SetsPlayerLose() {
+
+        BlackjackPlayer player = initiatePlayerWithTwoHands();
+        Hand firstHand = player.getHands().get(0);
+        Hand secondHand = player.getHands().get(1);
+
+        BlackjackGame game = createGame(player);
+        game.setSplitted(true);
+
+        Card cardKingDiamonds = new Card("KD", "TestUrl", "KING", "DIAMONDS");
+        Card cardQueenHearts = new Card("QH", "TestUrl", "QUEEN", "HEARTS");
+        secondHand.addCard(cardKingDiamonds);
+        secondHand.addCard(cardQueenHearts);
+
+        assertEquals(20, secondHand.getHandTotalNumber());
+        assertFalse(secondHand.isBust());
+        assertEquals("InGame", game.getResult());
+
+        Card cardTwoClubs = new Card("2C", "TestUrl", "2", "CLUBS");
+        MockDeck deck = new MockDeck(cardTwoClubs);
+        MockPresenter presenter = new MockPresenter();
+
+        PlayerHitInteractor interactor = new PlayerHitInteractor(deck, presenter, game);
+        PlayerHitInputData inputData = new PlayerHitInputData(player, true);
+
+        interactor.execute(inputData);
+
+        assertTrue(deck.drawCalled);
+        assertTrue(presenter.presentCalled);
+        assertSame(secondHand, presenter.outputData.getHandAfterHit());
+
+        assertTrue(secondHand.isBust());
+        assertTrue(presenter.outputData.isBust());
+        assertTrue(presenter.outputData.isSplitHand());
+
+        assertEquals("PlayerLose", game.getResult());
+    }
 }
