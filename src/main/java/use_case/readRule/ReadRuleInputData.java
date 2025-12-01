b7c6d226c8a468ddc4ecc68f4e405853f253b676
arindamdatta1024 <arindamdattabase@gmail.com>
2025-12-01 02:14:15 -0500

    Update Read rule controller, Input data, Presenter

--- src/main/java/use_case/readRule/ReadRuleInputData.java
+++ src/main/java/use_case/readRule/ReadRuleInputData.java
@@ -1,19 +1,10 @@
 package use_case.readRule;
 
-import entity.BlackjackGame;
-
 /**
  * The input or initial data needed for the read rule use case
  */
 public class ReadRuleInputData {
 
-    private final BlackjackGame blackjackGame;
+    public ReadRuleInputData() {}
 
-    public ReadRuleInputData(BlackjackGame blackjackGame) {
-        this.blackjackGame = blackjackGame;
-    }
-
-    public BlackjackGame getBlackjackGame() {
-        return blackjackGame;
-    }
 }
